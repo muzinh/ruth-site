27ecc8ca32be33871924a0d85d07f7e1b8e7637e
Murilo Donizete <murilodonizete18@gmail.com>
2026-06-06 03:00:28 -0300

    Adicionado Contador

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-document
-.getElementById("startBtn")
-.addEventListener("click", () => {
+const startBtn = document.getElementById("startBtn");
 
-    alert("Nossa história está apenas começando ❤️");
+startBtn.addEventListener("click", () => {
+
+    document
+    .getElementById("contador")
+    .scrollIntoView();
 
 });
+
+const dataInicio = new Date("2025-06-24T09:30:00");
+
+function atualizarContador(){
+
+    const agora = new Date();
+
+    const diferenca = agora - dataInicio;
+
+    const dias = Math.floor(
+        diferenca / (1000 * 60 * 60 * 24)
+    );
+
+    const horas = Math.floor(
+        (diferenca / (1000 * 60 * 60)) % 24
+    );
+
+    const minutos = Math.floor(
+        (diferenca / (1000 * 60)) % 60
+    );
+
+    const segundos = Math.floor(
+        (diferenca / 1000) % 60
+    );
+
+    document.getElementById("tempo").innerHTML =
+        `${dias} dias<br>
+         ${horas} horas<br>
+         ${minutos} minutos<br>
+         ${segundos} segundos`;
+
+}
+
+setInterval(atualizarContador, 1000);
+
+atualizarContador();
